41254fbd0ffcb48f6a0b52dbb6f8776572d30cc2
Camila-gallo <cgallopiva1@mail.austral.edu.ar>
2025-03-21 11:52:55 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    add= a + b
+    difference = a - b
+    product = a * b
+    average = (a + b)/2
+    intQuotient = a // b
+    remainder = a % b
+    division = a / a
+    print(add)
+    print(difference)
+    print(product)
+    print(average)
+    print(intQuotient)
+    print(remainder)
+    print(division)
